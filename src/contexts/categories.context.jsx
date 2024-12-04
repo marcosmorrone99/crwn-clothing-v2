8156c72cd4c17812dae2c963aacd86c6eaad1726
marcosmorrone99 <momarcos99@gmail.com>
2024-12-04 17:05:04 -0300

    categories redux

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
@@ -8,15 +8,6 @@ export const CategoriesContext = createContext({
 
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments('categories');
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
 
   const value = { categoriesMap };
   return (
